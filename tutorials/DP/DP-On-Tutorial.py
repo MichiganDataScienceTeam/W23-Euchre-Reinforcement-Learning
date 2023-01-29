aff5e293d54d534ff3bc13b2b04e88eaa2d7ab7e
Jakal02 <37743499+Jakal02@users.noreply.github.com>
2023-01-29 18:25:13 -0500

    Fixed DP Tutorial Bugs
    The DP tutorial now is ready to be attempted for this problem

--- tutorials/DP/DP-On-Tutorial.py
+++ tutorials/DP/DP-On-Tutorial.py
@@ -1,26 +1,51 @@
 '''
 Gridworld RL
 
+Problem:
+Try and find the optimal policy to navigate towards
+the goal state:
+    . . . .     Let . = 0
+    . . X .         X = 1
+    . . G X         G = 2
+    . X . .
+
+The locations as numbers are:
+    0  1  2  3
+    4  5  6  7
+    8  9  10 11
+    12 13 14 15
+
+TIP:
+    look in the init_reward_model and init_transition_matrix
+    functions for how to index into each of the respective dataframes
+    (use the loc func as well)
 '''
 
 import numpy as np
+import pandas as pd
 
 '''
 Global Variables
 '''
 sz = 4
 num_states = sz * sz
+actions = ["North","East","South","West"]
+num_actions = len(actions)
 terminal_state = 10
 terminal_reward = 10
 blocked_states = [6,11,13]
+non_terminal_states = [0,1,2,3,4,5,7,8,9,12,14,15]
 grid = None
-gamma = 0.3
+gamma = 0.8
 
 '''
-Global Functions
+Global Functions - Do not Touch
 '''
 
 def tup_2_st(coord:tuple):
+    """Turn coordinate tuple to location on number line.
+        E.g. (3,4) in a 5x5 grid = 3*5 + 4 = 19
+    """
     assert(coord[0] >= 0 and coord[0] < sz)
     assert(coord[1] >= 0 and coord[1] < sz)
     return coord[0]*sz + coord[1]
@@ -51,11 +76,9 @@ def grid_print(input,is_policy=False):
         if is_policy:
             for c in range(sz):
                 stuff = {0:"^",1:">",2:"v",3:"<"}
-                next_pos = np.argmax(input[tup_2_st((r,c)),:])
-                neighs = get_neighbors(tup_2_st((r,c)))
-                beep = [True if next_pos == n else False for n in neighs]
+                next_pos = np.argmax(input.loc[tup_2_st((r,c)),:].values)
                 if vals[grid[tup_2_st((r,c))]] == ".":
-                    print(stuff[np.argmax(beep)],end=" ")
+                    print(stuff[next_pos],end=" ")
                 else:
                     print(vals[grid[tup_2_st((r,c))]],end=" ")
         else:
@@ -65,14 +88,6 @@ def grid_print(input,is_policy=False):
     print()
     return
 
-def init_value_map():
-    v_map = np.zeros(num_states)
-    return v_map
-
-def init_reward_map():
-    r_map = np.zeros(num_states)
-    r_map[terminal_state] = 10 # get 10 reward 
-    return r_map
 
 def get_neighbors(state):
     '''
@@ -94,57 +109,155 @@ def get_neighbors(state):
         if res[i] in blocked_states:
             res[i] = state
     return res
-def init_policy():
-    '''
-    Create policy of size (sz^2,sz^2) \n
-    this maps state -> state transitions \n
-    init every direction as equal probability \n
-    '''
-    q_map = np.zeros((num_states,num_states))
-    for i in range(num_states):
-        neighbors = get_neighbors(i)
-        for n in neighbors:
-            q_map[(i,n)] += 0.25
-            
-    return q_map
 
+
+def init_value_map():
+    """
+    v_map: list of every possible state to fill with value
+
+    Note: this includes non-terminal states, but there values should always
+    be zero.
+    """
+    v_map = np.zeros(num_states)
+    return v_map
+
+
+def init_reward_map():
+    """
+    Rewards are a function of a (state, action) tuple  
+
+    reward map: Pandas dataframe with dims: (num_states[16], legal actions[4])
+
+    Note: this includes non-terminal states, but there values should always
+    be zero.
+    """
+    reward_map = pd.DataFrame(0, 
+                              index=non_terminal_states, 
+                              columns=actions)
+    reward_map.loc[9, "East"] = 10
+    reward_map.loc[14, "North"] = 10
+    return reward_map
+
+
+def init_policy(value):
+    '''
+    Create policy of size (num states, num actions)
+
+    this holds state -> action transitions  
+
+    Note: this includes non-terminal states, but there values should always
+    be zero.
+    '''
+    policy = pd.DataFrame(value, 
+                          index=range(num_states), 
+                          columns=actions)
+ 
+    return policy
+
+
+def init_random_policy():
+    """Give equal probability to each state"""
+    return init_policy(1/num_actions)
+
+
+def init_transition_dynamics():
+    """
+    Create matrix of (s,a) -> s_prime transitions
+    rows: (s,a) pair     columns: states
+    This will be 1 for all legal actions, and zero if you can't go there
+
+    Note: this includes non-terminal states, but there values should always
+    be zero.
+    """
+    # fill 0 for every (s,a) -> s_prime transition, even illegal moves
+    all_pairs = [(s, a) for s in range(num_states) for a in actions]
+    transition_matrix = pd.DataFrame(0, 
+                              index=pd.MultiIndex.from_tuples(all_pairs),
+                              columns=range(num_states))
+
+    # fill transition matrix with 1 if you can go there.
+    for s in non_terminal_states:
+        neighbors = get_neighbors(s)
+        for i in range(4): # for each action
+            # prob to get to new position from (s,a) tuple = 1
+            transition_matrix[neighbors[i]].loc[(s,actions[i])] = 1
+
+    return transition_matrix
 
 '''
 TODO Section Below
 '''
 
-
-def policy_evaluation(value_map,theta):
+def policy_evaluation(value_map, policy, rewards_model, transition_matrix, theta):
     '''
-    Returns: new (1,num_states) value_map
+    Implements step 2 in algorithm
 
+    Returns: new value_map
     '''
-    # TODO
+    while True:
+        delta = 0
+
+        for s in non_terminal_states:
+            # temp store value of V(s)
+            
+            # calculate new value for V(s)
+            
+            # save that value
+            
+            # change delta
+            pass
+        
+        if delta < theta:
+            break
+
     return value_map
 
-def policy_improvement(policy,value):
+
+def policy_improvement(value_map, policy, rewards_model, transition_matrix):
     '''
     Returns: policy, a (num_states,num_states) array containing transition probabilities from state -> state
              stable, bool indicating if policy == policy created by function end
+    Instead of comparing if a =/= pi(s). run through the loop and do:
+        if not policy.equals(new_policy)
     '''
     stable = True
-    # TODO
-    return policy, stable
+    new_policy = init_policy(0)
+
+    for s in non_terminal_states:
+        # skip a = pi(s) step
+        # find argmax over all 4 actions to take when in s
+        
+        # save that action in the new_policy
+        pass
+
+    # compare whole new_policy to old policy and set stable = False if not the same
+    
+    return new_policy, stable
+
 
 if __name__=="__main__":
     grid = initialize_grid1() # Exists only to support print statements
-    rewards = init_reward_map() # Never changes
-    theta = 0 # TODO
-    # --
-    value_map = init_value_map()
-    policy = init_policy()
-    print("Policy Iteration: Dynamic Programming")
-    
+    rewards_model = init_reward_map() # Never changes
+    transition_matrix = init_transition_dynamics() # Never changes
+    theta = 1e-4
 
     # --
     value_map = init_value_map()
-    policy = init_policy()
-    print("Policy Iteration: Closed Form")
-    
-    
+    policy = init_random_policy()
+    print("Policy Iteration: Dynamic Programming")
+    print("Initial Value Map")
+    grid_print(value_map)
+    print("Initial Policy")
+    grid_print(policy, True)
+
+    # --
+    # Begin algorithm here
+    num_steps = 0
+
+    #--
+    print("Final Value Map")
+    grid_print(value_map)
+    print("Final Policy Map")
+    grid_print(policy, True)
+    print(f"This problem took {num_steps} iterations to converge.")
     print("done")
